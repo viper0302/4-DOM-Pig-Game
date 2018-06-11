4f18c1a02c986f8816580e3210c3f5c57688fcf8
viper0302 <justin.j0302@gmail.com>
2018-06-11 12:06:35 -0600

    fixed if statement for scores.

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
         // Check if player won
-        if (scores[activePlayer] >= 10){
+        if (scores[activePlayer] >= 100){
             document.querySelector('#name-' + activePlayer).textContent = 'Winner';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
