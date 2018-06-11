27652a6aa8f220794aed36721a8c163a41c0c736
viper0302 <justin.j0302@gmail.com>
2018-06-11 12:05:48 -0600

    Finished Adding functionality to Pig game.

--- app.js
+++ app.js
@@ -8,21 +8,37 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-
-let scores, roundScores, activePlayer;
-
-scores = [0,0];
-roundScores = 0;
-activePlayer = 0;
+// Global Variables
+let scores, roundScores, activePlayer, gamePlaying;
 
 
+// Initializing function
+function init(){
+    scores = [0,0];
+    roundScores = 0;
+    activePlayer = 0;
+    gamePlaying = true;
 
-document.querySelector('.dice').style.display = 'none';
-document.getElementById('score-0').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.add('active');
+    document.querySelector('.btn-roll').style.display = 'block';
+    document.querySelector('.btn-hold').style.display = 'block';
+}
 
+init();
+
+
+
+
+// Next Player Function
  function nextPlayer(){
     // Next Players turn
     activePlayer === 0 ? activePlayer = 1 :  activePlayer = 0;
@@ -45,50 +61,56 @@ document.getElementById('current-1').textContent = '0';
 
 // Button Roll Function
 document.querySelector('.btn-roll').addEventListener('click', function () {
-    // 1. random number
-    let dice = Math.floor(Math.random() * 6) + 1;
+    if (gamePlaying){
+        // 1. random number
+        let dice = Math.floor(Math.random() * 6) + 1;
 
-    // 2. display the result
-    let diceDom = document.querySelector('.dice');
-    diceDom.style.display = 'block';
-    diceDom.src = 'dice-' + dice + '.png';
+        // 2. display the result
+        let diceDom = document.querySelector('.dice');
+        diceDom.style.display = 'block';
+        diceDom.src = 'dice-' + dice + '.png';
 
-    // 3. update the rolled number if the score is not 1
-    if (dice !== 1){
-        //Add score
-        roundScores += dice;
-        document.querySelector("#current-" + activePlayer).textContent = roundScores;
+        // 3. update the rolled number if the score is not 1
+        if (dice !== 1){
+            //Add score
+            roundScores += dice;
+            document.querySelector("#current-" + activePlayer).textContent = roundScores;
 
 
-    }else {
-        nextPlayer();
+        }else {
+            nextPlayer();
 
+        }
     }
+
 
 });
 
 // Hold Button Function
 document.querySelector('.btn-hold').addEventListener('click', function () {
+    if (gamePlaying){
+        // Add current score to global score
+        scores[activePlayer] += roundScores;
 
-    // Add current score to global score
-    scores[activePlayer] += roundScores;
+        // Update the UI
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
-    // Update the UI
-    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-
-    // Check if player won
-    if (scores[activePlayer] >= 10){
-        document.querySelector('#name-' + activePlayer).textContent = 'Winner';
-        document.querySelector('.dice').style.display = 'none';
-        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-        document.querySelector('.btn-roll').style.display = 'none';
-        document.querySelector('.btn-hold').style.display = 'none';
-    }else {
-        //Next Player
-        nextPlayer();
+        // Check if player won
+        if (scores[activePlayer] >= 10){
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            document.querySelector('.btn-roll').style.display = 'none';
+            document.querySelector('.btn-hold').style.display = 'none';
+            gamePlaying = false;
+        }else {
+            //Next Player
+            nextPlayer();
+        }
     }
 
-
-
 });
+
+// New Game Function
+document.querySelector('.btn-new').addEventListener('click', init);
